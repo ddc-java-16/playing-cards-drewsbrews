c777a0e940f71577ee49eab8a1fedaae35e7ff2f
drewsbrews <143551649+drewsbrews@users.noreply.github.com>
2023-09-25 16:29:36 -0600

    implemented draw and iterable

--- src/main/java/edu/cnm/deepdive/cards/model/Deck.java
+++ src/main/java/edu/cnm/deepdive/cards/model/Deck.java
@@ -2,11 +2,17 @@ package edu.cnm.deepdive.cards.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-public class Deck {
+public class Deck implements Iterable<Card> {
+
+
 
 private final List<Card> cards;
+
+private Iterator<Card> dealer;
 
   public Deck() {
     cards = new ArrayList<>();
@@ -17,15 +23,31 @@ private final List<Card> cards;
 
       }
     }
+    dealer = iterator();
   }
 
   public void shuffle() {
   Collections.shuffle(cards);
+dealer = iterator();
+  }
 
+  public Card draw () throws NoSuchElementException {
+    return dealer.next();
+  }
+
+  public boolean isEmpty() {
+    return !dealer.hasNext();
   }
 
   @Override
   public String toString() {
     return cards.toString();
   }
+
+  @SuppressWarnings("NullableProblems")
+  @Override
+  public Iterator<Card> iterator() {
+    return cards.iterator();
+  }
+
 }
