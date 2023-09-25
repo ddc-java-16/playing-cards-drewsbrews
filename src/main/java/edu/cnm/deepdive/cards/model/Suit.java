8f4b321ad46981f5e952ba83da9cdc5a2d958f76
drewsbrews <143551649+drewsbrews@users.noreply.github.com>
2023-09-25 14:07:28 -0600

    basic classes for playing cards

---
+++ src/main/java/edu/cnm/deepdive/cards/model/Suit.java
@@ -0,0 +1,35 @@
+package edu.cnm.deepdive.cards.model;
+
+@SuppressWarnings({"UnnecessaryUnicodeEscape", "unused"})
+public enum Suit {
+
+
+CLUBS('\u2663', Color.BLACK),
+
+DIAMONDS('\u2662', Color.RED),
+
+HEARTS('\u2661', Color.RED),
+
+SPADES('\u2660', Color.BLACK);
+
+private final char symbol;
+private final Color color;
+
+Suit (char symbol, Color color){
+  this.symbol = symbol;
+  this.color = color;
+}
+
+  public char getSymbol() {
+    return symbol;
+  }
+
+  public Color getColor() {
+    return color;
+  }
+
+  public enum Color {
+
+  BLACK, RED
+}
+}
